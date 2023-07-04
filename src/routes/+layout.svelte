ef68af04dde3012571e5a9f3e560f3c4ba5fddda
Tony Bailey <tony@tonybailey.dev>
2023-07-04 20:46:10 +0100

    Add a layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<h1>Svelte Playground</h1>
+<nav>
+	<a href="/">Home</a>
+	<a href="/uuid">UUID Generator</a>
+    <a href="/lights-off">Lights Off!</a>
+</nav>
+
+<hr />
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,4 @@
-<h1>Svelte Playground</h1>
-<p>Projects:</p>
-<ul>
-    <li><a href="./uuid">UUID Generator</a></li>
-    <li><a href="./lights-off">Lights off!</a></li>
-</ul>
+<h2>Home</h2>
+<p>Welcome to my Svelte playground!</p>
+<p>Try one of the projects.</p>
+
