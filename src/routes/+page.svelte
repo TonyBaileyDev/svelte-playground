c9925f43a26db52eba4887e97d815c409fedf731
Tony Bailey <tony@tonybailey.dev>
2023-07-05 20:30:57 +0100

    Back to a multi-page app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,11 @@
 <h2>Home</h2>
 <p>Welcome to my Svelte playground!</p>
 <p>Try one of the projects.</p>
+<ul>
+    <li><a href="/uuid">UUID Generator</a></li>
+    <li><a href="/lights-off">Lights Off!</a></li>
+    <li><a href="/lights-off-2">More Lights Off!</a></li>
+    <li><a href="/adventure">Adventure</a></li>
+</ul>
 
+<p>By <a href="https://www.tonybailey.dev/">Tony Bailey</a></p>
